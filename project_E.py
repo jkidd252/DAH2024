7a997e32568f2e6e5801240da9f82f46d1531523
Jonathan Kidd <113980446+jkidd252@users.noreply.github.com>
2024-11-05 16:49:51 +0000

    Add files via upload

--- project_E.py
+++ project_E.py
@@ -8,9 +8,15 @@ from matplotlib import pyplot as plt
 tmp_b = DS18B20( address="28-000005e9a98b")
 tmp_w = DS18B20( address="28-00000d7cf96a")
 tmp_ref = DS18B20( address="10-000803471e5e")
+tmp_ref2 = DS18B20( address="10-000802de0f29")
+
 GPIO.setmode(GPIO.BCM)
 Switch = 17
 GPIO.setup(Switch, GPIO.OUT)
+#time.sleep(1)
+#GPIO.output(Switch, GPIO.HIGH)
+#time.sleep(10) 
+#GPIO.output(Switch, GPIO.LOW)
 
 # list to store values for plotting
 temp_b = []
@@ -23,7 +29,7 @@ temp_ref.append(tmp_ref.getCelsius())
 time_l.append(time.time())
 while len(time_l) < 16:
 	GPIO.output(Switch, GPIO.HIGH) 
-	time.sleep(1)
+	time.sleep(5)
 	print('black: '+str(tmp_b.getCelsius())+', white: '+str(tmp_w.getCelsius())+', ref: '+str(tmp_ref.getCelsius()))
 	temp_b.append(tmp_b.getCelsius())
 	temp_w.append(tmp_w.getCelsius())
@@ -34,14 +40,14 @@ while len(time_l) < 16:
 
 fig, ax1 = plt.subplots()
 
-ax1.plot(time_l, temp_b, label='black')
-ax1.plot(time_l, temp_w, label='white')
+ax1.plot(time_l, temp_b, label='black', color='r')
+ax1.plot(time_l, temp_w, label='white', color='blue')
 ax1.legend()
 ax1.set_xlabel('Time [Second]')
 ax1.set_ylabel('Temp [Degree C]')
 
 ax2 = ax1.twinx()
-ax2.plot(time_l, temp_ref, label='ref')
+ax2.plot(time_l, temp_ref, label='ref', color='green')
 ax2.legend()
 ax2.set_ylabel('Temp [Degree C]')
 fig.tight_layout()
